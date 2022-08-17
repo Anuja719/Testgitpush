fd9ab27a8403276be4e40fb73e24672eb26183c8
Anuja719 <anujaghadmode3@gmail.com>
2022-08-17 18:13:00 +0530

    this is my second code push in git

--- mongotest.py
+++ mongotest.py
@@ -8,6 +8,12 @@ d = {
     "emailid" : "anuja@ineuron.ai",
     "surname" : "Chalisgaonkar"
 }
+
+d = {
+    "name" : "Anuja",
+    "emailid" : "anuja@ineuron.ai",
+    "surname" : "Chalisgaonkar"
+}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
